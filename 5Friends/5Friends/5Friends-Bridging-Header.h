a880979e7c24688e9469b6e8c9f58bc339c9141e
Jesse Sum <jesse.sum@lookout.com>
2015-05-27 00:16:22 -0700

    5FriendsApp

---
+++ 5Friends/5Friends/5Friends-Bridging-Header.h
@@ -0,0 +1,11 @@
+//
+//  5Friends-Bridging-Header.h
+//  5Friends
+//
+//  Created by Jesse Sum on 5/26/15.
+//  Copyright (c) 2015 Jesse Sum. All rights reserved.
+//
+
+#import <Firebase/Firebase.h>
+#import "JSQMessages.h"
+#import "JSQMessageData.h"
---
+++ ios-swift-chat-example/5Friends/5Friends-Bridging-Header.h
@@ -0,0 +1,3 @@
+#import <Firebase/Firebase.h>
+#import "JSQMessages.h"
+#import "JSQMessageData.h"
